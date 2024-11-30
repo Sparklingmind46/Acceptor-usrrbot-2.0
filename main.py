9d000450a45ca8fe738ea7d6e7a11f394da48069
Sparklingmind46 <sparklingmind42@gmail.com>
2024-11-30 11:59:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,86 +1,57 @@
-from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from pyrogram import Client, filters
-from pyrogram.types import *
-from motor.motor_asyncio import AsyncIOMotorClient  
-from os import environ as env
-import asyncio, datetime, time
+import asyncio
+import logging
+import logging.config
+import warnings
+from pyrogram import Client
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from config import Config
+from aiohttp import web
+from pytz import timezone
+from datetime import datetime
+from plugins.web_support import web_server
 
 
-ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅"
-START_TEXT = "Hii {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use"
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
-API_ID = int(env.get('API_ID'))
-API_HASH = env.get('API_HASH')
-BOT_TOKEN = env.get('BOT_TOKEN')
-DB_URL = env.get('DB_URL')
-ADMINS = int(env.get('ADMINS'))
 
-Dbclient = AsyncIOMotorClient(DB_URL)
-Cluster = Dbclient['Cluster0']
-Data = Cluster['users']
-Bot = Client(name='AutoAcceptBot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-       
-      
-     
-@Bot.on_message(filters.command("start") & filters.private)                    
-async def start_handler(c, m):
-    user_id = m.from_user.id
-    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
-    button = [[        
-        InlineKeyboardButton('Updates Channel', url='https://t.me/Team_SAT_25')
-    ]]
-    return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
-          
+class Bot(Client):
 
-@Bot.on_message(filters.command(["broadcast", "users"]) & filters.user(ADMINS))  
-async def broadcast(c, m):
-    if m.text == "/users":
-        total_users = await Data.count_documents({})
-        return await m.reply(f"Total Users: {total_users}")
-    b_msg = m.reply_to_message
-    sts = await m.reply_text("Broadcasting your messages...")
-    users = Data.find({})
-    total_users = await Data.count_documents({})
-    done = 0
-    failed = 0
-    success = 0
-    start_time = time.time()
-    async for user in users:
-        user_id = int(user['id'])
-        try:
-            await b_msg.copy(chat_id=user_id)
-            success += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            await b_msg.copy(chat_id=user_id)
-            success += 1
-        except InputUserDeactivated:
-            await Data.delete_many({'id': user_id})
-            failed += 1
-        except UserIsBlocked:
-            failed += 1
-        except PeerIdInvalid:
-            await Data.delete_many({'id': user_id})
-            failed += 1
-        except Exception as e:
-            failed += 1
-        done += 1
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}")    
-    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.delete()
-    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
+    def __init__(self):
+        super().__init__(
+            name="AutoAcceptBot",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
+        )
 
- 
-@Bot.on_chat_join_request()
-async def req_accept(c, m):
-    user_id = m.from_user.id
-    chat_id = m.chat.id
-    if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
-    await c.approve_chat_join_request(chat_id, user_id)
-    try: await c.send_message(user_id, ACCEPTED_TEXT.format(user=m.from_user.mention, chat=m.chat.title))
-    except Exception as e: print(e)
-   
-   
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, Config.PORT).start()
+        logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
 
-Bot.run()
+        if Config.ADMIN:
+            try:
+                await self.send_message(Config.ADMIN, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
+            except:
+                pass
+
+    async def stop(self, *args):
+        await super().stop()
+        logging.info("Bot Stopped 🙄")
+
+
+bot = Bot()
+
+bot.run()
